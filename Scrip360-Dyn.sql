e85ed77b79cc5492cdca3de1db773495e1cf75c2
Jackson Barreto <jackson.barreto@gmail.com>
2022-02-12 16:41:48 +0000

    add dates create and update in chanel table

--- Scrip360-Dyn.sql
+++ Scrip360-Dyn.sql
@@ -23,8 +23,8 @@ VALUES ('DynamanWMS-2-Sanitop360', 'DynamanWMS', 'Sanitop360', current_timestamp
 
 -- CONFIGURAÇÃO DOS CANAIS 
 insert into SYM_CHANNEL (channel_id, processing_order, max_batch_size, max_batch_to_send,
-                         extract_period_millis, batch_algorithm, enabled, reload_flag, description)
-values ('chanelDynaman', 2, 1000, 10, 0, 'default', 1, 1, 'Dynaman WMS data chanel');
+                         extract_period_millis, batch_algorithm, enabled, reload_flag, description, last_update_time, create_time)
+values ('chanelDynaman', 2, 1000, 10, 0, 'default', 1, 1, 'Dynaman WMS data chanel', current_timestamp, current_timestamp);
 
 
 -- CONFIGURAÇÃO DOS TRIGGERS (TABELAS A SEREM MONITORADAS)
