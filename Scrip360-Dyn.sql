9db57952b032c633e2eddab52043831377cd29b3
Jackson Barreto <jackson.barreto@gmail.com>
2022-02-28 17:28:29 +0000

    add sync from table: container inventory

--- Scrip360-Dyn.sql
+++ Scrip360-Dyn.sql
@@ -146,7 +146,7 @@ INSERT INTO SYM_TRIGGER
 VALUES 
 (
     'tablesDynaman',
-    'task_detail,stock_product,picklist_detail_order,outbound_order_header,outbound_Order_Detail,inbound_Order_Header,arrival_detail,arrival_header,inbound_Order_detail,outbound_Order_text',
+    'task_detail,stock_product,picklist_detail_order,Container_Inventory,outbound_order_header,outbound_Order_Detail,inbound_Order_Header,arrival_detail,arrival_header,inbound_Order_detail,outbound_Order_text',
     'chanelDynaman', 
     current_timestamp, 
     current_timestamp,
@@ -507,3 +507,339 @@ VALUES
     'Installation',
     'Changes the name of the source table on the target.'
  );
+ Container_Inventory
+ INSERT INTO sym_transform_table 
+(
+    transform_id, 
+    source_node_group_id, 
+    target_node_group_id, 
+    transform_point, 
+    source_table_name, 
+    target_table_name, 
+    delete_action, 
+    update_action, 
+    column_policy, 
+    create_time, 
+    last_update_time,
+    last_update_by,
+    description
+)
+VALUES 
+(
+    'Container_InventoryChangeName',
+    'DynamanWMS',
+    'Sanitop360',
+    'EXTRACT',
+    'Container_Inventory',
+    'Dyn56_Container_Inventory',
+    'DEL_ROW',
+    'UPD_ROW',
+    'SPECIFIED',
+    current_timestamp,
+    current_timestamp,
+    'Installation',
+    'Changes the name of the source table on the target.'
+ );
+
+
+ --Transform Column
+ INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'locationCodeContainerTransform', 
+        '*', 
+        'location_code', 
+        'location_code', 
+        0,
+        'copy', 
+        '', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
+ INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'containerCodeContainerTransform', 
+        '*', 
+        'container_code', 
+        'Container_code', 
+        1,
+        'copy', 
+        '', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
+ INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'inventoryQtyContainerTransform', 
+        '*', 
+        'inventory_qty', 
+        'inventory_qty', 
+        0,
+        'copy', 
+        '', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
+INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'qtyOnPicklistContainerTransform', 
+        '*', 
+        'qty_on_picklist', 
+        'qty_on_picklist', 
+        0,
+        'copy', 
+        '', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
+INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'qtyNotOnPicklistContainerTransform', 
+        '*', 
+        'qty_not_on_picklist', 
+        'qty_not_on_picklist', 
+        0,
+        'copy', 
+        '', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
+INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'pickedQtyContainerTransform', 
+        '*', 
+        'picked_qty', 
+        'picked_qty', 
+        0,
+        'copy', 
+        '', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
+INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'handlingQtyContainerTransform', 
+        '*', 
+        'handling_qty', 
+        'handling_qty', 
+        0,
+        'copy', 
+        '', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
+INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'itemCodeContainerTransform', 
+        '*', 
+        'item_code', 
+        '', 
+        0,
+        'lookup', 
+        'SELECT item_code from stock_product WHERE stock_product_id = :stock_product_id', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
+INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'equipmentZoneCodeContainerTransform', 
+        '*', 
+        'equipment_zone_code', 
+        '', 
+        0,
+        'lookup', 
+        'SELECT equipment_zone_code from location WHERE warehouse_code = :warehouse_code AND location_code = :location_code', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
+INSERT INTO SYM_TRANSFORM_COLUMN 
+ (
+        transform_id, 
+        include_on, 
+        target_column_name, 
+        source_column_name, 
+        pk,
+        transform_type, 
+        transform_expression, 
+        transform_order, 
+        last_update_time,
+        last_update_by, 
+        create_time,
+        description
+) 
+VALUES 
+(
+        'stockTypeContainerTransform', 
+        '*', 
+        'StockType', 
+        '', 
+        0,
+        'lookup', 
+        'SELECT CASE WHEN Location_code IN ('1BRICO', 'OF', 'KITOF', 'BLISPACK', 'BLISPACKO', 'PINT', 'PVNOVA','PVNOVAO') THEN 'OF' WHEN Location_code IN ('LOST', 'UNDO') THEN 'LOST' WHEN Location_code LIKE ('GER%') THEN 'LOST' WHEN Location_code LIKE ('SGUR%') THEN 'LOST' WHEN Location_code LIKE ('AGUARD%') THEN 'WAIT' WHEN Location_code LIKE ('4CAIS%') THEN 'WAIT' WHEN Location_code IN ('01W0', '01W1', '02W0', '02W1', '02W2', '04W0', '04W1', '6P01W0', '06P02W0') THEN 'CAIS' WHEN Location_code IN ('04DOC01', '04DOC02', '04DOC03', '04DOC04', '04DOC05', '04DOCSOS', '01DOC01', '01DOC02', '01DOCNEI', '01DOCSAV') THEN 'CAIS' WHEN Location_code = '1REC01' THEN 'REC' WHEN Location_code LIKE ('1CAI%') THEN 'CAIS' ELSE 'STOCK' END AS StockType from location WHERE warehouse_code = :warehouse_code AND location_code = :location_code', 
+        1, 
+        current_timestamp, 
+        'Installation',
+        current_timestamp,
+        'simple copy from source column to destination column.'
+);
